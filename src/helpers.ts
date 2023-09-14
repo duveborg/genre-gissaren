f1ff7732885546f819141f3183cfb490e0a94378
duveborg <duveborg@gmail.com>
2023-09-14 20:51:49 +0000

    Deploying to gh-pages from @ duveborg/genre-gissaren@2e68531b05a15f4ecd19b44d68988737de0f3d87 🚀

--- src/helpers.ts
+++
@@ -1,7 +0,0 @@
-export const corsProxyUrl = (originalUrl: string) =>
-  'https://corsproxy.io/?' + encodeURIComponent(originalUrl);
-  
-export const imageUrl = (url: string) =>
-  corsProxyUrl(`https://imageproxy.a2d.tv?width=400&source=${url}`);
-
-export const sortRandom = () => Math.random() - 0.5;
